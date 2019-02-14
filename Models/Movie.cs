b7e55b6f9b5a4dc982e7c1aff090c83f35ce68b2
Chirag Sehra <chiragsehra42@gmail.com>
2019-02-14 13:21:49 +0530

    Add files via upload

---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // these are just a set of properties to descibe a movie
+
+    }
+}
